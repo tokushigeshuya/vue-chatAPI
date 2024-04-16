a00754563b9beda29f2dfdaddc6c472a92b1d021
tokushigeshuya <narihare1130@gmail.com>
2024-04-16 19:20:08 +0900

    [fix]コントローラの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
     def test
-        render json:{ message: 'This is a test message from Rails' }
+        render json: { message: 'This is a test message from Rails 202404' }
     end
 end
---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,19 @@
+class RoomsController < ApplicationController
+    def index
+        # Room モデルの全てのデータを取得して、JSON形式でレスポンスを返します。
+        rooms = Room.all
+        render json: rooms, status: ok
+    end
+
+    def create
+        # Room モデルに新しいデータを作成して、JSON形式でレスポンスを返します。
+        room = Room.create(room_params)
+        render json: room, status: :created
+    end
+
+    private
+
+    def room_params
+        params.require(:room).permit(:name)
+    end
+end
